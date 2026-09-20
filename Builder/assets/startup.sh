3b3626ebb5507ef0e943ecbbda29e9bdfe62ad90
aartvi <artfilend@gmail.com>
2026-09-20 13:42:11 +0300

    Update version number in startup script

--- Builder/assets/startup.sh
+++ Builder/assets/startup.sh
@@ -6,7 +6,7 @@ default="\033[1;0m"
 
 clear
 
-printf "${cb} aArtVI bspwm-dotfiles\n\n"
-printf "${cg} Version: 1.6\n"
+printf "${cb} aArtVI bspwm-dotfiles\n"
+printf "${cg} Version: 1.7\n"
 #printf "${cg} Youtube: https://youtube.com/@zproger"
 printf "${default}\n\n"
