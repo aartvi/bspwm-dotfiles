84a3492bf356610effbc30a40cb4f79fcc1f4bdf
aartvi <artfilend@gmail.com>
2026-09-20 09:26:02 +0300

    Update startup.sh for version and branding changes

--- Builder/assets/startup.sh
+++ Builder/assets/startup.sh
@@ -6,9 +6,7 @@ default="\033[1;0m"
 
 clear
 
-printf "${cb} ┌─┐┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐┬┬  ┌─┐┌─┐\n"
-printf "${cb} ┌─┘├─┘├┬┘│ ││ ┬├┤ ├┬┘   │││ │ │ ├┤ ││  ├┤ └─┐\n"
-printf "${cb} └─┘┴  ┴└─└─┘└─┘└─┘┴└─  ─┴┘└─┘ ┴ └  ┴┴─┘└─┘└─┘\n\n"
-printf "${cg} Version: 1.3\n"
-printf "${cg} Youtube: https://youtube.com/@zproger"
+printf "${cb} aArtVI bspwm-dotfiles\n\n"
+printf "${cg} Version: 1.6\n"
+#printf "${cg} Youtube: https://youtube.com/@zproger"
 printf "${default}\n\n"
